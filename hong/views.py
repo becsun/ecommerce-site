6ffce0ec075e99e7da410da88101e959b820b1e8
bec <becsun@icloud.com>
2020-11-23 13:04:34 +0000

    forms made with django forms instead of HTML

--- hong/views.py
+++ hong/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
   context = {
     "title":"Hello World!",
@@ -9,23 +11,27 @@ def home_page(request):
   return render(request, "home_page.html", context)
 
 def about_page(request):
-    context = {
+  context = {
     "title":"About Page!",
     "content":"Welcome to the about page."
   }
-    return render(request, "home_page.html", context)
+  return render(request, "home_page.html", context)
 
 def contact_page(request):
-    context = {
+  contact_form = ContactForm(request.POST or None)
+  context = {
     "title":"Contact!",
-    "content":"Welcome to the contact Page."
-    }
-    if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
-    return render(request, "contact/view.html", context)
+    "content":"Welcome to the contact Page.",
+    "form":contact_form
+  }
+  if contact_form.is_valid():
+    print(contact_form.cleaned_data)
+  # if request.method == "POST":
+      # print(request.POST)
+      # print(request.POST.get('fullname'))
+      # print(request.POST.get('email'))
+      # print(request.POST.get('content'))
+  return render(request, "contact/view.html", context)
 
 
 def home_page_old(request):
